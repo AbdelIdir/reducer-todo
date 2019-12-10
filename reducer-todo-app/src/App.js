4fb77483131ee3619c563775cd7c2b210fe595fe
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-10 14:29:22 +0100

    Made a form,initial states

--- reducer-todo-app/src/App.js
+++ reducer-todo-app/src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import uuid from "uuid";
 function App() {
+  const EmptyinitialStateTodoList = [
+    { id: uuid(), task: "", completed: false }
+  ];
+  const todoList = [{ id: uuid(), task: "do some workout ", completed: false }];
+  const initialSate = {
+    todoList
+  };
+
+const MAKE_A_TODO_NOTE = "MAKE_A_TODO_NOTE";
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>
+            TODO LIST APP WITH REDUCER
+            <input type="text" name="task" />
+            <button>submit your task</button>
+          </label>
+        </form>
       </header>
     </div>
   );
